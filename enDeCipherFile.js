6547ab3daf78cb8ee41db979b87e55d4278fb791
haya alghazouli <haya-ron@hotmail.com>
2023-03-22 23:56:52 -0400

    Logout button added to enDeCipherFile.html

--- enDeCipherFile.js
+++ enDeCipherFile.js
@@ -1,4 +1,16 @@
 let user = document.getElementById("user");
+let logout = document.getElementById("logout");
 let userInfo = JSON.parse(window.localStorage.getItem("user"));
 
+console.log(user)
 user.innerHTML = userInfo.firstName
+
+
+logout.addEventListener("click", e => {
+    e.preventDefault();
+    console.log("click")
+    window.localStorage.removeItem("user");
+    window.location.replace("LoginPage.html");
+    
+    
+})
