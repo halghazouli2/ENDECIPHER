ed19c28e91745061280e6d39be2a6edfad7f0b0c
Haya Alghazouli <hayaalghazouli@Hayas-MBP.attlocal.net>
2023-05-03 19:59:39 -0400

    Fixed Code

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
+// Get the email input field and password input field elements from the HTML document
 const loginEmail = document.getElementById("login-email");
 const loginPassword = document.getElementById("login-password");
+
+// Get the login button element from the HTML document
 const loginBtn = document.getElementById("login-submit");
+
+// Get the error message elements for username and password fields from the HTML document
 let userNameError = document.getElementById("user-name-error");
 let passwordError = document.getElementById("password-error");
 
+// Add an event listener to the login button element that listens for a click
 loginBtn.addEventListener("click", async (e) => {
+  // Prevent the default behavior of the click event (submitting a form)
   e.preventDefault();
+
+  // Get the values entered in the email and password input fields
   let emailValue = loginEmail.value;
   let passwordValue = loginPassword.value;
-  
+
+  // Create an object with the email and password values
   let user = {
     userName : emailValue,
     password: passwordValue
   }
 
+  // Send a POST request to the specified endpoint with the user object as the request body
   const response = await fetch("https://endecipherapi.onrender.com/login", {
     method: "POST",
-     headers: {
+    headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(user)
   })
+
+  // Wait for the response from the server and parse the response body as JSON
   const userRes = await response.json();
+
+  // If the server response status is "ok", log the user object to the console, alert the user that they have logged in, redirect them to another page, and save the user object to local storage
   if(userRes.status === "ok"){
     console.log(userRes.user)
-
     alert("login successfully");
     window.location.href = "EnDeCipherFile.html"
     window.localStorage.setItem("user", JSON.stringify(userRes.user))
 
+  // If the server response status is not "ok", check the error message in the response and display the appropriate error message on the page
   }else{
-
+    // If the error message is "invalid password", display an error message for the password field and hide the error message for the username field
     if(userRes.error === "invalid password"){
       passwordError.innerHTML = userRes.error
       passwordError.style.display = "block"
       userNameError.style.display = "none"
 
+    // If the error message is "this userName does not exist, Please Signup.", display an error message for the username field and hide the error message for the password field
     }else if(userRes.error === "this userName does not exist, Please Signup."){
-
       userNameError.innerHTML = userRes.error
       userNameError.style.display = "block"
       passwordError.style.display = "none"
-
     }
-
   }
-
-
 })
--- signup.js
+++ signup.js
@@ -1,15 +1,17 @@
+//getting all the elements needed from the HTML 
 const fn = document.getElementById("fn");
 const ln = document.getElementById("ln");
 const un = document.getElementById("un");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const btn = document.getElementById("btn");
+//getting the error elements needed from the HTML 
 let fnError = document.getElementById("first-name-error");
 let lnError = document.getElementById("last-name-error");
 let unError = document.getElementById("user-name-error");
 let emailError = document.getElementById("email-error");
 let passwordError = document.getElementById("password-error");
-
+//adding an event listener to the button, which will execute when it is clicked
 btn.addEventListener("click", async (e) => {
   e.preventDefault();
   let fnValue = fn.value;
@@ -17,7 +19,7 @@ btn.addEventListener("click", async (e) => {
   let unValue = un.value;
   let emailValue = email.value;
   let passwordValue = password.value;
-
+//creating a user object with the values obtained from the input fields
   let user = {
     firstName: fnValue,
     lastName: lnValue,
@@ -26,6 +28,7 @@ btn.addEventListener("click", async (e) => {
     password: passwordValue
   }
 
+  //sending a POST request to the server with the user object as the request body
   const response = await fetch("https://endecipherapi.onrender.com/signup", {
     method: "POST",
      headers: {
@@ -35,11 +38,14 @@ btn.addEventListener("click", async (e) => {
   })
   const userRes = await response.json()
   console.log(userRes)
+  //checking the response from the server
   if(userRes.status === "ok"){
+    // If the status is ok, we are alerting the user that a new user has been signed up successfully
     alert("signup a new user");
     window.location.href = "EnDeCipherFile.html";
     window.localStorage.setItem("user", JSON.stringify(userRes.user))
   }else {
+    // If there was an error, we are displaying the appropriate error message on the webpage
     if(userRes.error.email !== ""){
       emailError.innerHTML = userRes.error.email;
       emailError.style.display = "block";
