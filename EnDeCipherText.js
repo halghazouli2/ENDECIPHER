75f63c5234a3c7efc4d155fd8dea29ed8718e0bb
Haya Alghazouli <hayaalghazouli@Hayas-MBP.attlocal.net>
2023-05-03 02:45:32 -0400

    Fixed code

--- EnDeCipherText.js
+++ EnDeCipherText.js
@@ -1,6 +1,9 @@
+
+// Declare the alphabet and its length
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const alphabetLength = alphabet.length;
 
+// Get the input elements and output elements
 const textInput = document.getElementById("text");
 const passwordInput = document.getElementById("password");
 const encryptBtn = document.getElementById("encryptBtn");
@@ -8,9 +11,13 @@ const decryptBtn = document.getElementById("decryptBtn");
 const encryptedOutput = document.getElementById("encrypted");
 const decryptedOutput = document.getElementById("decrypted");
 
+// Declare variables to store the ciphertext bytes and IV
 let ciphertextBytes, iv;
 
+// Function to encrypt the plaintext
 async function encrypt() {
+
+  // Get the plaintext and password inputs
   const plaintext = textInput.value;
   const password = passwordInput.value;
   if (password.length < 16) {
@@ -18,9 +25,11 @@ async function encrypt() {
     return;
   }
 
+  // Convert the plaintext and password to bytes
   const plaintextBytes = new TextEncoder().encode(plaintext);
   const passwordBytes = new TextEncoder().encode(password);
 
+  // Import the password bytes as a key and derive a new key
   const key = await crypto.subtle.importKey("raw", passwordBytes, "PBKDF2", false, ["deriveKey"]);
   const derivedKey = await crypto.subtle.deriveKey({
     name: "PBKDF2",
@@ -29,17 +38,23 @@ async function encrypt() {
     hash: "SHA-256"
   }, key, { name: "AES-GCM", length: 256 }, false, ["encrypt"]);
 
+  // Generate a random IV
   iv = crypto.getRandomValues(new Uint8Array(12));
 
+  // Encrypt the plaintext using the derived key and IV
   const ciphertext = await crypto.subtle.encrypt({ name: "AES-GCM", iv: iv }, derivedKey, plaintextBytes);
 
+  // Convert the ciphertext bytes to a hexadecimal string
   ciphertextBytes = new Uint8Array(ciphertext);
   const ciphertextHex = Array.from(ciphertextBytes).map(b => b.toString(16).padStart(2, "0")).join("");
 
+  // Set the encrypted output value to the ciphertext hexadecimal string
   encryptedOutput.value = ciphertextHex;
 }
 
+// Function to decrypt the ciphertext
 async function decrypt() {
+  // Import the password bytes as a key and derive the same key as used in encryption
   const key = await crypto.subtle.importKey("raw", new TextEncoder().encode(passwordInput.value), "PBKDF2", false, ["deriveKey"]);
   const derivedKey = await crypto.subtle.deriveKey({
     name: "PBKDF2",
@@ -48,21 +63,26 @@ async function decrypt() {
     hash: "SHA-256"
   }, key, { name: "AES-GCM", length: 256 }, false, ["decrypt"]);
 
+  // Decrypt the ciphertext using the derived key and IV
   const decryptedBytes = await crypto.subtle.decrypt({ name: "AES-GCM", iv: iv }, derivedKey, ciphertextBytes);
   const decryptedPlaintext = new TextDecoder().decode(decryptedBytes);
 
+  // Set the decrypted output value to the decrypted plaintext
   decryptedOutput.value = decryptedPlaintext;
 }
 
+// Add event listeners to the encrypt and decrypt buttons
 encryptBtn.addEventListener("click", encrypt);
 decryptBtn.addEventListener("click", decrypt);
 
+// Get the user element and display the user's first name
 let user = document.getElementById('userr')
 let userInfo = JSON.parse(window.localStorage.getItem("user"));
 console.log(userInfo)
 user.innerHTML = userInfo.firstName
   let logout = document.getElementById("logoutt");
   
+  // Get the logout button and remove the user from local storage 
   logout.addEventListener("click", e => {
     e.preventDefault();
     console.log("click");
--- enDeCipherFile.js
+++ enDeCipherFile.js
@@ -1,25 +1,39 @@
 
     
+    // Declare variables 'mode' and 'objFile' and initialize them to null
     var mode=null;
     var objFile=null;
+
+    // Call the 'switchdiv()' function with an argument 'encrypt' to set the default div to be shown
     switchdiv('encrypt');
 
+    // Define the 'switchdiv()' function to switch between encrypt and decrypt divs
     function switchdiv(t) {
+        // If the argument is 'encrypt'
         if(t=='encrypt') {
+            // Show the div for encrypt file and hide the div for decrypt file
             divEncryptfile.style.display='block';
             divDecryptfile.style.display='none';
+            // Disable the encrypt button and enable the decrypt button
             btnDivEncrypt.disabled=true;
             btnDivDecrypt.disabled=false;
+              // Set the mode variable to 'encrypt'
             mode='encrypt';
+
+             // If the argument is 'decrypt'
         } else if(t=='decrypt') {
+
+            // Show the div for decrypt file and hide the div for encrypt file
             divEncryptfile.style.display='none';
             divDecryptfile.style.display='block';
+            // Enable the encrypt button and disable the decrypt button
             btnDivEncrypt.disabled=false;
             btnDivDecrypt.disabled=true;
+            // Set the mode variable to 'decrypt'
             mode='decrypt';
         }
     }
-
+    // This function performs validation for encryption process.
     function encvalidate() {
         if (txtEncpassphrase.value.length >= 16 && txtEncpassphrase.value == txtEncpassphraseretype.value) { 
           spnCheckretype.classList.add("greenspan");
@@ -49,7 +63,7 @@
         if( txtDecpassphrase.value.length>0 && objFile ) { btnDecrypt.disabled=false; } else { btnDecrypt.disabled=true; }
     }
 
-
+    //This function is an event handler for the "drop" event. When a file is dropped onto a designated drop zone, this function is called. The function first prevents the default behavior of the event 
     function drop_handler(ev) {
         console.log("Drop");
         ev.preventDefault();
@@ -78,30 +92,30 @@
     function dragover_handler(ev) {
         console.log("dragOver");
 
-        ev.preventDefault();
+        ev.preventDefault(); // Prevents the default action of the dragged item
     }
 
     function dragend_handler(ev) {
         console.log("dragEnd");
 
         var dt = ev.dataTransfer;
-        if (dt.items) {
+        if (dt.items) { // If the dataTransfer object contains items
 
-            for (var i = 0; i < dt.items.length; i++) {
-                dt.items.remove(i);
+            for (var i = 0; i < dt.items.length; i++) { // Loop through the items
+                dt.items.remove(i); // Remove the item at the specified index from the items list
             }
-        } else {
+        } else { // If the dataTransfer object does not contain items
 
-            ev.dataTransfer.clearData();
+            ev.dataTransfer.clearData(); // Clears the data associated with the dragged item from the dataTransfer object
         }
     }
-
+//selectfile function is called when the user selects a file. It assigns the first file in the Files array to the objFile variable and calls the displayfile function to display information about the selected file.
     function selectfile(Files) {
         objFile=Files[0];
         displayfile()
         if(mode=='encrypt') { encvalidate(); } else if(mode=='decrypt') { decvalidate(); }
     }
-
+//displayfile function displays the name and size of the selected file, along with a unit (bytes, KB, MB, etc.). It checks the value of the mode variable to determine whether to display the information for encryption or decryption.
     function displayfile() {
         var s;
         var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
@@ -115,7 +129,7 @@
             spndecfilename.textContent=objFile.name + ' (' + s + ')'; 
         } 
     }
-
+//readfile function returns a Promise that reads the contents of a file as an array buffer. It creates a new FileReader object and sets its onload property to a function that resolves the Promise with the result property of the FileReader object, which contains the contents of the file. The FileReader object then reads the file as an array buffer.
     function readfile(file){
         return new Promise((resolve, reject) => {
             var fr = new FileReader();  
@@ -124,21 +138,25 @@
             };
             fr.readAsArrayBuffer(file);
         });
+    
+
     }
 
     async function encryptfile() {
-        btnEncrypt.disabled=true;
+        btnEncrypt.disabled=true; // Disable the encrypt button while encryption is in progress
 
+        // Read the selected file and store its contents as an array of bytes
         var plaintextbytes=await readfile(objFile)
         .catch(function(err){
             console.error(err);
         }); 
         var plaintextbytes=new Uint8Array(plaintextbytes);
-
+        // Set up parameters for the PBKDF2 key derivation function
         var pbkdf2iterations=10000;
         var passphrasebytes=new TextEncoder("utf-8").encode(txtEncpassphrase.value);
         var pbkdf2salt=window.crypto.getRandomValues(new Uint8Array(8));
 
+        // Import the passphrase as a raw key, then use it to derive a PBKDF2 key
         var passphrasekey=await window.crypto.subtle.importKey('raw', passphrasebytes, {name: 'PBKDF2'}, false, ['deriveBits'])
         .catch(function(err){
             console.error(err);
@@ -152,20 +170,23 @@
         console.log('pbkdf2bytes derived');
         pbkdf2bytes=new Uint8Array(pbkdf2bytes);
 
+        // Extract the first 32 bytes of the derived key as the encryption key,
+        // and the remaining bytes as the initialization vector (IV)
         keybytes=pbkdf2bytes.slice(0,32);
         ivbytes=pbkdf2bytes.slice(32);
 
+        //This code is responsible for encrypting a file using the AES-CBC encryption algorithm.
         var key=await window.crypto.subtle.importKey('raw', keybytes, {name: 'AES-CBC', length: 256}, false, ['encrypt']) 
         .catch(function(err){
             console.error(err);
         });
         console.log('key imported');        
-
+        // Encrypt the plaintext file data using the imported key and IV
         var cipherbytes=await window.crypto.subtle.encrypt({name: "AES-CBC", iv: ivbytes}, key, plaintextbytes)
         .catch(function(err){
             console.error(err);
         });
-
+        // If encryption fails, display an error message and return
         if(!cipherbytes) {
             spnEncstatus.classList.add("redspan");
             spnEncstatus.innerHTML='<p>Error encrypting file.  See console log.</p>';
@@ -174,55 +195,57 @@
 
         console.log('plaintext encrypted');
         cipherbytes=new Uint8Array(cipherbytes);
-
+        // Save the encrypted data and salt in a new byte array
         var resultbytes=new Uint8Array(cipherbytes.length+16)
         resultbytes.set(new TextEncoder("utf-8").encode('Salted__'));
         resultbytes.set(pbkdf2salt, 8);
         resultbytes.set(cipherbytes, 16);
 
+        // Create a new Blob object representing the encrypted file
         var blob=new Blob([resultbytes], {type: 'application/download'});
         var blobUrl=URL.createObjectURL(blob);
         aEncsavefile.href=blobUrl;
         aEncsavefile.download=objFile.name + '.enc';
 
+        // Update the HTML to display a success message and provide a download link
         spnEncstatus.classList.add("greenspan");
         spnEncstatus.innerHTML='<p>File encrypted.</p>';
         aEncsavefile.hidden=false;
     }
 
     async function decryptfile() {
-        btnDecrypt.disabled=true;
+        btnDecrypt.disabled=true; // Disable the "Decrypt" button.
 
-        var cipherbytes=await readfile(objFile)
+        var cipherbytes=await readfile(objFile) // Read the selected file as an array of bytes.
         .catch(function(err){
             console.error(err);
         }); 
-        var cipherbytes=new Uint8Array(cipherbytes);
+        var cipherbytes=new Uint8Array(cipherbytes); // Convert the array buffer to a Uint8Array.
 
-        var pbkdf2iterations=10000;
-        var passphrasebytes=new TextEncoder("utf-8").encode(txtDecpassphrase.value);
-        var pbkdf2salt=cipherbytes.slice(8,16);
+        var pbkdf2iterations=10000; // The number of iterations for PBKDF2.
+        var passphrasebytes=new TextEncoder("utf-8").encode(txtDecpassphrase.value); // Convert the passphrase to a byte array.
+        var pbkdf2salt=cipherbytes.slice(8,16); // Extract the salt from the cipher bytes.
 
 
-        var passphrasekey=await window.crypto.subtle.importKey('raw', passphrasebytes, {name: 'PBKDF2'}, false, ['deriveBits'])
+        var passphrasekey=await window.crypto.subtle.importKey('raw', passphrasebytes, {name: 'PBKDF2'}, false, ['deriveBits']) // Import the passphrase as a key using PBKDF2.
         .catch(function(err){
             console.error(err);
 
         });
         console.log('passphrasekey imported');
 
-        var pbkdf2bytes=await window.crypto.subtle.deriveBits({"name": 'PBKDF2', "salt": pbkdf2salt, "iterations": pbkdf2iterations, "hash": 'SHA-256'}, passphrasekey, 384)        
+        var pbkdf2bytes=await window.crypto.subtle.deriveBits({"name": 'PBKDF2', "salt": pbkdf2salt, "iterations": pbkdf2iterations, "hash": 'SHA-256'}, passphrasekey, 384) // Derive a key using PBKDF2.       
         .catch(function(err){
             console.error(err);
         });
         console.log('pbkdf2bytes derived');
-        pbkdf2bytes=new Uint8Array(pbkdf2bytes);
+        pbkdf2bytes=new Uint8Array(pbkdf2bytes); // Convert the derived key to a Uint8Array.
 
-        keybytes=pbkdf2bytes.slice(0,32);
-        ivbytes=pbkdf2bytes.slice(32);
-        cipherbytes=cipherbytes.slice(16);
+        keybytes=pbkdf2bytes.slice(0,32); // Extract the first 32 bytes of the derived key as the encryption key.
+        ivbytes=pbkdf2bytes.slice(32); // Extract the remaining bytes of the derived key as the initialization vector.
+        cipherbytes=cipherbytes.slice(16); // Remove the salt from the cipher bytes.
 
-        var key=await window.crypto.subtle.importKey('raw', keybytes, {name: 'AES-CBC', length: 256}, false, ['decrypt']) 
+        var key=await window.crypto.subtle.importKey('raw', keybytes, {name: 'AES-CBC', length: 256}, false, ['decrypt']) // Decrypt the cipher bytes. 
         .catch(function(err){
             console.error(err);
         });
@@ -240,13 +263,16 @@
         }
 
         console.log('ciphertext decrypted');
-        plaintextbytes=new Uint8Array(plaintextbytes);
-
+        // Convert the decrypted bytes to a Uint8Array.
+        plaintextbytes=new Uint8Array(plaintextbytes); 
+        // Create a new Blob object with the decrypted bytes.
         var blob=new Blob([plaintextbytes], {type: 'application/download'});
+        // Create a URL for the Blob.
         var blobUrl=URL.createObjectURL(blob);
+        // Set the download link's href attribute to the URL.
         aDecsavefile.href=blobUrl;
         aDecsavefile.download=objFile.name + '.dec';
-
+        // Add the "greenspan" class to the status span to make it green.
         spnDecstatus.classList.add("greenspan");
         spnDecstatus.innerHTML='<p>File decrypted!</p>';
         aDecsavefile.hidden=false;
